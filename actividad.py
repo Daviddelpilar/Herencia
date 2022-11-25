3a43dc62f683c72c091424d1b1fe6c3525155106
Daviddelpilar <117951699+Daviddelpilar@users.noreply.github.com>
2022-11-25 12:55:12 +0100

    Add files via upload

---
+++ actividad.py
@@ -0,0 +1,14 @@
+class Producto():
+    def __init__(self,nombre,unidades,precio):
+        self.nombre=nombre
+        self.unidades=unidades
+        self.precio=precio
+    def total(self):
+        print(f'el total es {self.precio *self.unidades}')
+
+
+producto1=Producto('camisa',15,9.95)
+producto2=Producto('chaqueta',20,19.95)
+
+producto1.total()
+producto2.total()
